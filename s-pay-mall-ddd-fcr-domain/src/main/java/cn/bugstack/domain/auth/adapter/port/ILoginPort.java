53e42b05846251946850eb2ec7f67e427bcbad01
sxlva <xiaolv@lvlvjiangdeMacBook-Air.local>
2025-07-26 16:12:08 +0800

    feat: 微信登陆实现

---
+++ s-pay-mall-ddd-fcr-domain/src/main/java/cn/bugstack/domain/auth/adapter/port/ILoginPort.java
@@ -0,0 +1,14 @@
+package cn.bugstack.domain.auth.adapter.port;
+
+import java.io.IOException;
+
+/**
+ * @author xiaolv
+ * @date 2025/7/26 15:07
+ * @description 定义微信登陆外部接口实现标准
+ */
+public interface ILoginPort {
+    String createQrCodeTicket() throws IOException;
+
+    void sendLoginTemplate(String openid) throws IOException;
+}
