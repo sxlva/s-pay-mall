941379b7440e000bfe865b6761c89dec73d2cb52
sxlva <xiaolv@lvlvjiangdeMacBook-Air.local>
2025-07-29 16:40:25 +0800

    feat: 订单的实现

--- s-pay-mall-ddd-fcr-infrastructure/src/main/java/cn/bugstack/infrastructure/gateway/ProductRPC.java
+++ s-pay-mall-ddd-fcr-infrastructure/src/main/java/cn/bugstack/infrastructure/gateway/ProductRPC.java
@@ -1,6 +1,7 @@
 package cn.bugstack.infrastructure.gateway;
 
 import cn.bugstack.infrastructure.gateway.dto.ProductDTO;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
  * @date 2025/7/28 22:37
  * @description
  */
+@Service
 public class ProductRPC {
 
     public ProductDTO queryProductByProductId(String productId){
